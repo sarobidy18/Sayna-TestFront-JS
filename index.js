4e44a25597cb9b6cc3d291c24420b846edcd7eb0
Sarobidy RANAIVOSAMINANA <sarobidymanana18@gmail.com>
2021-12-04 22:15:19 +0100

    with assets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+console.log(path.join(__dirname, "./public"))
 app.use(express.static(path.join(__dirname, "public")))
 app.set("views", path.join(__dirname, "./public"))
 app.set("view engine", "html");
