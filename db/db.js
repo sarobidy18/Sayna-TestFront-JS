b23629e05c4709b1a49489a25f239cbe9e69998d
Sarobidy RANAIVOSAMINANA <sarobidymanana18@gmail.com>
2021-12-04 22:22:47 +0100

    with assets

--- db/db.js
+++ db/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
+const var_mongo = process.env.MONGODB_URI;
+// const var_mongo = config.database;
+
 module.exports = function() {
-    console.log(config.database);
-    mongoose.connect(config.database, {
+    mongoose.connect(var_mongo, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    }).then(() => console.log(`Connected to DB : ${config.database}`))
+    }).then(() => console.log(`Connected to DB : ${var_mongo}`))
     .catch((err => {
         console.log("DB Error :",err)
         if (err.code == 'EHOSTUNREACH') {
